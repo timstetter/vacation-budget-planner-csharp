26ac45629d40fdb42fab89aae75f7a028cb8dd3e
timstetter <tstetter@gmail.com>
2021-04-18 15:26:58 -0400

    Week 1 first commit

---
+++ VacationBudgetPlanner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VacationBudgetPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print a message to the user that welcomes them to your application
+
+            Console.WriteLine("Welcome to Tim's Vacation Budget Planner!\n");
+
+
+            //Prompt the user for their name
+
+            Console.WriteLine("Please enter your name:");
+            string userName = Console.ReadLine();
+
+            /*
+            * Prompt the user with a welcome message that greets them by name 
+            * and asks them from which of the two options would they like to travel
+            */
+            Console.WriteLine($"Hello {userName}!  Where would you like to go, Mexico or Japan?");
+
+            //End of Week 1 steps.
+
+            //Leave the console open
+            Console.ReadLine();
+        }
+    }
+}
